7b0a7797001dc5a66f636ac9e8821d5a3cd884a3
Moeeze Hassan <fammas.maz@gmail.com>
2023-02-06 11:54:07 +0100

    [BASE]: Fix the plotting function
    The now primalSchur script plots the dislacement of the nodes, against the node number as well as the length of the beam
    
    Signed-off-by: Moeeze Hassan <fammas.maz@gmail.com>

--- base/internalNodes.m
+++ base/internalNodes.m
@@ -1,12 +1,19 @@
-function [ui, u] = internalNodes(truss, reshapeNodes, Sp, bp, ub)
+function [ui, u, uif, unf] = internalNodes(truss, reshapeNodes, Sp, bp, ub)
 
 ui = [];
 u = [];
+unf = [];
+uif = [];
 for i=1:truss.nbSub
     [~, ~, Kii, Kib, fi] = fem_k(truss, 0);
-    ui = [ui; Kii\(fi - (Kib)*ub(i:i+1))];
+    uil = Kii\(fi - (Kib)*ub(i:i+1));
+    ui = [ui; uil];
     u = [u; ub(i); Kii\(fi - (Kib)*ub(i:i+1));];
+    unf = [unf; ub(i); zeros(length(uil),1)];
+    uif = [uif; 0; Kii\(fi - (Kib)*ub(i:i+1));];
 end
 u = [u; ub(end)];
+unf = [unf; ub(end)];
+uif = [uif; 0];
 end
 
